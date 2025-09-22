0177e8613a9484d677997fb1f4ed01bf6e77f75a
jasminaberdibekova114-collab <jasminaberdibekova114@gmail.com>
2025-09-22 16:49:29 +0500

    some changes

--- dars.php
+++ dars.php
@@ -1,5 +1,12 @@
 <?php
 
-class jasmina{ 
-    echo "hello jasmina"
+class marjona{
+    echo "jasmina"
+    public function salomdunyo(){
+        echo "salom dunyo";
+    }
+public function FunctionName($firstname,$lastname){
+    return $firstname + $lastname;
 }
+
+}     
